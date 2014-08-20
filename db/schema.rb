00e09a5ec4f45f3a8dc6d34d5b4857217d2ad514
Donald Cuttie <donald@cuttie.com>
2014-08-19 23:08:51 -0400

    Implemented Devise for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140816144558) do
+ActiveRecord::Schema.define(version: 20140817014736) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 20140816144558) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
