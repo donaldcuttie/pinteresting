7866399911fce49f0bf14f800414f91194f37efb
Donald Cuttie <donald@cuttie.com>
2014-08-20 16:18:03 -0400

    Trying to fix precompiling error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Pinteresting::Application.configure do
 
   #Required for Heroku
   #Note to set this to your actual host
-  config.action_mailer.default_url_options = { host: => 'pinteresting-commits.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'pinteresting-commits.herokuapp.com' }
 end
