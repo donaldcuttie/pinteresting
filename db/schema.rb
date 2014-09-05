f27a33596a9fc6cb23887577380acaebcfd8f57c
Donald Cuttie <donald@cuttie.com>
2014-09-04 23:38:16 -0400

    Replaced email with user's name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831032014) do
+ActiveRecord::Schema.define(version: 20140905024314) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140831032014) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
